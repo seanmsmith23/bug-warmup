c456897b14c11a0e83af44979a3dbca78c95ece6
Sean Smith <seansmith1020@gmail.com>
2014-07-15 09:26:47 -0600

    user sees flash when not selecting is_hunter

--- app.rb
+++ app.rb
@@ -20,14 +20,23 @@ class App < Sinatra::Base
   end
 
   post "/registrations" do
-    insert_sql = <<-SQL
-      INSERT INTO users (username, email, password, name_is_hunter)
-      VALUES ('#{params[:username]}', '#{params[:email]}', '#{params[:password]}', '#{params[:name_is_hunter]}')
-    SQL
+    is_hunter = params[:name_is_hunter]
+    p "IS HUNTER #{is_hunter}"
 
-    @database_connection.sql(insert_sql)
-    flash[:notice] = "Thanks for signing up"
+    if is_hunter.to_i == 1
+      insert_sql = <<-SQL
+        INSERT INTO users (username, email, password, name_is_hunter)
+        VALUES ('#{params[:username]}', '#{params[:email]}', '#{params[:password]}', '#{params[:name_is_hunter]}')
+      SQL
 
-    redirect "/"
+      @database_connection.sql(insert_sql)
+      flash[:notice] = "Thanks for signing up"
+      redirect "/"
+    else
+      flash[:notice] = "Ummm... Hunters only... Go change your name nerd."
+      redirect "/register"
+    end
+
+
   end
 end
